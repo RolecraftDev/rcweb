420b72ebe40d2f718714345f61df79a9c764b165
LaughingQuoll <LaughingQuoll@users.noreply.github.com>
2014-08-10 18:14:51 +1000

    Update index.php
    Added members

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-guild  = isset($_GET['g']) ? $_GET['g'] : 'None';
+$guild = isset($_GET['g']) ? $_GET['g'] : 'None';
 ?>
 
 <div class='container'>
@@ -32,7 +32,7 @@ $row = mysql_fetch_assoc($result);
 
 echo "
    <div class='info'>
-    <img src='http://mcapi.ca/v2/avatar/?player="$face".$row['leader']."&size=110' class='avatar' style=''/>
+    <img src='http://mcapi.ca/v2/avatar/?player=".$row['leader']."&size=110' class='avatar' style=''/>
   </div>
 
   <div class='other'>
@@ -64,6 +64,37 @@ echo "".$row['influence']."";
             </blockquote>
           </div>
       </div>
+      <table class="table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+ 
+<?php
+   $server = ""; //host
+   $dbuser = ""; //User Name
+   $dbpass = ""; //Password
+   $dbname = ""; //Database name
+   
+mysql_connect($server, $dbuser, $dbpass);
+mysql_select_db($dbname);
+mysql_query("SET NAMES 'utf8'");
+$query="SELECT * FROM `guildtable` WHERE `guild` LIKE 'guild' LIMIT 1";
+$result=mysql_query($query);
+while($row = mysql_fetch_assoc($result)){
+echo "       <tr>
+         
+          <td>".$row['members']."</td>
+        </tr>";
+}
+?>
+      </tbody>
+    </table>
 <center>
 <li class="disabled"><p></p></li>
           <li class="disabled">© 2014 LaughingQuoll</li>

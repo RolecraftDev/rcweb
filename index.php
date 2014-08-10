9a733d62b791af80a46e9e3fe71885271be94081
LaughingQuoll <LaughingQuoll@users.noreply.github.com>
2014-08-10 18:08:57 +1000

    Update index.php
    More stuff

--- index.php
+++ index.php
@@ -25,13 +25,15 @@ echo "
 mysql_connect($server, $dbuser, $dbpass);
 mysql_select_db($dbname);
 mysql_query("SET NAMES 'utf8'");
-$query="SELECT * FROM `guilds` WHERE `guild` LIKE 'guild' LIMIT 1";
+$query="SELECT * FROM `guildtable` WHERE `guild` LIKE 'guild' LIMIT 1";
 $result=mysql_query($query);
 $row = mysql_fetch_assoc($result);
 
 
 echo "
-  
+   <div class='info'>
+    <img src='http://mcapi.ca/v2/avatar/?player="$face".$row['leader']."&size=110' class='avatar' style=''/>
+  </div>
 
   <div class='other'>
     <h1 title='guild'>guild</h1>";
@@ -44,9 +46,27 @@ echo "
 </div>
     <br><h1></h1>
 
+      <div class="row">
+          <div class="col-sm-3">
+            <h6>Leader</h6>
+            <blockquote><p>
+             <?php
+echo "".$row['leader']."";
+?></p>          </blockquote>
+          </div>
+          <div class="col-sm-3">
+            <h6>Influence</h6>
+            <blockquote>
+              <p>
+     <?php
+echo "".$row['influence']."";
+?> </p>
+            </blockquote>
+          </div>
+      </div>
 <center>
 <li class="disabled"><p></p></li>
-          <li class="disabled">© 2014 QuollCraft</li>
+          <li class="disabled">© 2014 LaughingQuoll</li>
         </center>
 
     
